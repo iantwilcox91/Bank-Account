59b57551866a8cf45dc6c686ff935bdaee64ccd8
Ian Wilcox <ian@>
2016-08-25 17:49:02 -0700

    added some stuff

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// BUISNESS LOGIC
+function BankAccountConstructor(name) {
+this.name = name;
+this.money = [];
+}
+
+function Money (balance, deposit, withdrawl){
+  this.balance = balance;
+  this.deposit = deposit
+  this.withdrawl = withdrawl
+}
+
+
+
+
+
+
+
+
+
+
+
+// FRONT END LOGIC
